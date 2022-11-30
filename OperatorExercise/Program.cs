f011546d9edebb077e2b27c212466450a3ffc870
schavis2022 <schavis2022>
2022-11-29 21:53:01 -0500

    Operator Exercise Radius Finished

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,30 +1,55 @@
-﻿namespace OperatorExercise
+﻿using static System.Formats.Asn1.AsnWriter;
+using System.Reflection.Metadata;
+using System.Threading;
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
+using System.Runtime.Intrinsics.X86;
+using System;
+
+namespace OperatorExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            //OperationExercise1
-            //For division, create two integer variables called a and b.
+            //Operator Exercise 1
 
-            //var a = 17; //var is inferred and int. is Explicit typing.
-            //int b = 4;  //int. is Explicit typing.
-            //var name =  //name is a reference type of string.
+            //Console.ReadLine();
 
-            var a = 41;
-            var b = 17;
-            var addition = a + b;   
+            var a = 17;
+            var b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
 
-
-            //var a = 17;
-            //var b = 4;
-            //var quotient = a / b;
-            //var remainder = a % b;
-
-            Console.WriteLine(a +  "/"  + b + " is " + quotient + " remainder " + remainder);
-            // The + symbol is what concanation/pulls everthing together to form the string.
             Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
 
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+            Console.Write(AreaOfCircle(radius));
+
+            //Operator Exercise 2
+            //Create a method that will Calculate the area of a circle based on its radius - you can name the method AreaOfCircle.
+            //This method will use this formula Math.PI* Math.Pow(radius, 2) and return the area of type double
+            //The method will also accept radius as a parameter
+            //Allow a user to input a value for radius in the console using the following code:
+
+            //Define a method - which performs some functionality.
+            //that can be used over and over again.
         }
+          
+            public static double AreaOfCircle(double radius)
+
+            {
+                //This is the scope of the method
+                var area = Math.PI * Math.Pow(radius, 2);
+
+                return area;
+            }
+        }
+
     }
-}
+
+
